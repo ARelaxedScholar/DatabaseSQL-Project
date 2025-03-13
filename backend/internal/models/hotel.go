41627a228af7708e8a9f48fefb1ee2a0eab1f51d
Kouakou Jedidiah Ange-Emmanuel <angemmanuel.kouakou+professional@gmail.com>
2025-03-13 11:14:44 +0000

    feat(hotel.go): The model is done pretty much?
    The constructors and whatnot were created, and at this point I think everything is properly validated.
    
    Potential QOL improvements would be to make the errors be aggregated and return a slice of errors instead of going one at a time.
    
    But for now, this will do.

--- backend/internal/models/hotel.go
+++ backend/internal/models/hotel.go
@@ -1,10 +1,12 @@
 package models
 
+import "errors"
+
 // Amenities Enum
-type Amenities int
+type Amenity int
 
 const (
-	WIFI Amenities = iota + 1
+	WIFI Amenity = iota + 1
 	TV
 	AC
 	MiniFridge
@@ -20,6 +22,28 @@ const (
 	Office
 )
 
+func (self Amenity) isValid() bool {
+	switch self {
+	case WIFI,
+		TV,
+		AC,
+		MiniFridge,
+		CoffeeMachine,
+		AirDryer,
+		Safe,
+		Jacuzzi,
+		Balcony,
+		RoomService,
+		KingSizeBed,
+		QueenSizeBed,
+		SimpleBed,
+		Office:
+		return true
+	default:
+		return false
+	}
+}
+
 // View Enum
 type ViewType int
 
@@ -31,6 +55,15 @@ const (
 	Courtyard
 	Pool
 )
+
+func (self ViewType) isValid() bool {
+	switch self {
+	case Sea, Mountain, City, Park, Courtyard, Pool:
+		return true
+	default:
+		return false
+	}
+}
 
 // RoomType
 type RoomType int
@@ -45,6 +78,22 @@ const (
 	DeluxeSuite
 	FamilialSuite
 )
+
+func (self RoomType) isValid() bool {
+	switch self {
+	case Simple,
+		Double,
+		Twin,
+		Queen,
+		King,
+		JuniorSuite,
+		DeluxeSuite,
+		FamilialSuite:
+		return true
+	default:
+		return false
+	}
+}
 
 type HotelChain struct {
 	id, numberOfHotel                      int
@@ -63,6 +112,141 @@ type Room struct {
 	viewType                     ViewType
 	roomType                     RoomType
 	isExtensible                 bool
+	amenities                    map[Amenity]struct{} // Hashset of Go (I know stupid)
 }
 
 // todo: Define the constructors for this
+func NewHotelChain(id, numberOfHotel int, name, centralAddress, email, telephone string) (*HotelChain, error) {
+	var err error
+	// Validate Fields
+	switch {
+	case id < 0:
+		err = errors.New("Hotel chain's ID cannot be negative.")
+	case numberOfHotel < 0:
+		err = errors.New("Number of hotels cannot be negative.")
+	case name == "":
+		err = errors.New("Hotel chain's name cannot be empty.")
+
+	case centralAddress == "":
+		err = errors.New("Central address cannot be empty.")
+
+	case email == "":
+		err = errors.New("Hotel chain's email cannot be empty.")
+
+	case telephone == "":
+		err = errors.New("Hotel chain's phone Number cannot be empty.")
+
+	}
+
+	// if a case was hit
+	if err != nil {
+		return nil, err
+	}
+
+	// else just return
+	return &HotelChain{
+		id:             id,
+		numberOfHotel:  numberOfHotel,
+		name:           name,
+		centralAddress: centralAddress,
+		email:          email,
+		telephone:      telephone,
+	}, nil
+}
+
+func NewHotel(id, chainId, rating, numberOfRooms int,
+	name, address, email, telephone string) (*Hotel, error) {
+	var err error
+	// Validate Fields
+	switch {
+	case id < 0:
+		err = errors.New("Hotel's ID cannot be negative.")
+	case chainId < 0:
+		err = errors.New("Hotel chain's ID cannot be negative.")
+	case rating < 1:
+		err = errors.New("Hotel's rating must be at least 1.")
+	case rating > 5:
+		err = errors.New("Hotel's rating can be at most 5.")
+	case numberOfRooms < 1:
+		err = errors.New("Hotel must have at least one room.")
+	case name == "":
+		err = errors.New("Hotel's name cannot be empty")
+	case address == "":
+		err = errors.New("Hotel's address cannot be empty")
+	case email == "":
+		err = errors.New("Hotel's email cannot be empty")
+	case telephone == "":
+		err = errors.New("Hotel's phone Number cannot be empty")
+	}
+
+	// if a case was hit
+	if err != nil {
+		return nil, err
+	}
+
+	// else just return
+	return &Hotel{
+		id:            id,
+		chainId:       chainId,
+		rating:        rating,
+		numberOfRooms: numberOfRooms,
+		name:          name,
+		address:       address,
+		email:         email,
+		telephone:     telephone,
+	}, nil
+
+}
+
+func NewRoom(id, hotelId, capacity, floor int,
+	price float64,
+	telephone string,
+	viewType ViewType,
+	roomType RoomType,
+	isExtensible bool,
+	amenities map[Amenity]struct{}) (*Room, error) {
+	var err error
+	// Validate Fields
+	switch {
+	case id < 0:
+		err = errors.New("Room's ID cannot be negative.")
+	case hotelId < 0:
+		err = errors.New("Hotel's ID cannot be negative.")
+	case capacity < 1:
+		err = errors.New("Room's capacity must be at least 1.")
+	case price < 0:
+		err = errors.New("Room's price cannot be negative.")
+	case telephone == "":
+		err = errors.New("Room's phone Number cannot be empty")
+	case !viewType.isValid():
+		err = errors.New("Invalid variant of view type was passed to constructor")
+	case !roomType.isValid():
+		err = errors.New("Invalid variant of room type was passed to constructor")
+	}
+
+	// validate amenities
+	for k, _ := range amenities {
+		if !k.isValid() {
+			err = errors.New("The set of amenities contains an invalid variant")
+			break
+		}
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &Room{
+		id:           id,
+		hotelId:      hotelId,
+		capacity:     capacity,
+		floor:        floor,
+		price:        price,
+		telephone:    telephone,
+		viewType:     viewType,
+		roomType:     roomType,
+		isExtensible: isExtensible,
+		amenities:    amenities,
+	}, nil
+
+}
