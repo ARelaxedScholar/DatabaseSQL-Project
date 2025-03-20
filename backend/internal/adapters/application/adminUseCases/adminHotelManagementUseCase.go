25297b1fd11318c39f5280abf262dfbf28f7c9ed
Kouakou Jedidiah Ange-Emmanuel <angemmanuel.kouakou+professional@gmail.com>
2025-03-20 19:56:11 +0000

    feat(adminHotelManagementUseCase.go)

---
+++ backend/internal/adapters/application/adminUseCases/adminHotelManagementUseCase.go
@@ -0,0 +1,54 @@
+package adminUseCases
+
+import (
+	"github.com/sql-project-backend/internal/models/dto"
+	"github.com/sql-project-backend/internal/ports"
+)
+
+type DefaultAdminHotelManagementUseCase struct {
+	hotelService ports.HotelService
+}
+
+func NewAdminHotelManagementUseCase(hotelService ports.HotelService) ports.AdminHotelManagementUseCase {
+	return &DefaultAdminHotelManagementUseCase{
+		hotelService: hotelService,
+	}
+}
+
+func (uc *DefaultAdminHotelManagementUseCase) AddHotel(input dto.HotelInput) (dto.HotelOutput, error) {
+	hotel, err := uc.hotelService.AddHotel(
+		input.ID,
+		input.ChainID,
+		input.Rating,
+		input.NumberOfRooms,
+		input.Name,
+		input.Address,
+		input.Email,
+		input.Phone,
+	)
+	if err != nil {
+		return dto.HotelOutput{}, err
+	}
+	return dto.HotelOutput{HotelID: hotel.ID}, nil
+}
+
+func (uc *DefaultAdminHotelManagementUseCase) UpdateHotel(input dto.HotelInput) (dto.HotelOutput, error) {
+	hotel, err := uc.hotelService.UpdateHotel(
+		input.ID,
+		input.ChainID,
+		input.Rating,
+		input.NumberOfRooms,
+		input.Name,
+		input.Address,
+		input.Email,
+		input.Phone,
+	)
+	if err != nil {
+		return dto.HotelOutput{}, err
+	}
+	return dto.HotelOutput{HotelID: hotel.ID}, nil
+}
+
+func (uc *DefaultAdminHotelManagementUseCase) DeleteHotel(hotelID int) error {
+	return uc.hotelService.DeleteHotel(hotelID)
+}
